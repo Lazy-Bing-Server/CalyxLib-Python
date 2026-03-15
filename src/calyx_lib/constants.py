374f8ccf7bfaef93604e4c82cdb46aa4f10e8c41
Ra1ny_Yuki <ra1ny_yuki@outlook.com>
2026-03-15 21:32:53 +0800

    Add TestPyPI publish action

--- src/calyx_lib/constants.py
+++ src/calyx_lib/constants.py
@@ -4,7 +4,8 @@ NAME = 'CalyxLib'
 PACKAGE_NAME = 'calyx_lib'
 
 VERSION = '0.1.0-alpha.5'
-__version__ = '0.1.0a5'
+# Requires "" here to make regex work in publish action
+__version__ = "0.1.0a5"
 VERSION_PYPI = __version__
 
 GITHUB_URL = 'https://github.com/Lazy-Bing-Server/CalyxLib-Python'
