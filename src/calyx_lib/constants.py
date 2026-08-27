cb3e167da95c8b0ef6a3896f9f0de2c1f96a009a
Ra1ny_Yuki <ra1ny_yuki@outlook.com>
2026-08-28 00:27:20 +0800

    Bump version to v0.1.0a6 to avoid testpypi limits

--- src/calyx_lib/constants.py
+++ src/calyx_lib/constants.py
@@ -3,9 +3,9 @@ import os
 NAME = 'CalyxLib'
 PACKAGE_NAME = 'calyx_lib'
 
-VERSION = '0.1.0-alpha.5'
+VERSION = '0.1.0-alpha.6'
 # Requires "" here to make regex work in publish action
-VERSION_PYPI = "0.1.0a5"
+VERSION_PYPI = "0.1.0a6"
 
 GITHUB_URL = 'https://github.com/Lazy-Bing-Server/CalyxLib-Python'
 
